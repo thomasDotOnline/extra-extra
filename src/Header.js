e018520e59bc5b864d6f787feffbf73c29c78be3
thomasDotOnline <t.garrison333@gmail.com>
2020-12-05 13:09:40 -0500

    first push of extra extra react installed

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import react, { Component } from 'react'
+
+
+class Header extends Component {
+    render() {
+        return(
+            <h1>Front Page</h1>
+        );
+    }
+};
+export default Header;
